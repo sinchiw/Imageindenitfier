9021364c9f54a4d81ce54d2ab0eb4350948c6ddd
Wilmer <wilmersinchi@gmail.com>
2019-07-07 15:08:06 -0400

    Initial Commit

---
+++ ImageRegonizerTests/ImageRegonizerTests.swift
@@ -0,0 +1,34 @@
+//
+//  ImageRegonizerTests.swift
+//  ImageRegonizerTests
+//
+//  Created by Wilmer sinchi on 7/7/19.
+//  Copyright © 2019 Wilmer sinchi. All rights reserved.
+//
+
+import XCTest
+@testable import ImageRegonizer
+
+class ImageRegonizerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
